440b7a3f87cf112d26b076f8ba30131dddec92e9
lisfan <goolisfan@gmail.com>
2017-10-25 19:40:50 +0800

    代码分享

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import { cube } from './math.js';
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
 function component() {
   var element = document.createElement('pre');
 
   element.innerHTML = [
     'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
+    '1 cubed is equal to ' + cube(5)
   ].join('\n\n');
 
   return element;
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 const webpack = require('webpack')
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    index: './src/index.js',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
   },
-  devtool: 'inline-source-map',
+  // devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
-    hot: true
+    // hot: true
   },
   module: {
     rules: [
@@ -48,6 +49,7 @@ module.exports = {
       title: 'Output Management'
     }),
     new ManifestPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    // new webpack.HotModuleReplacementPlugin(),
+    new UglifyJSPlugin()
   ],
 };
