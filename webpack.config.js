7d880e0afc0639ec711c755995acd3feedff368c
lisfan <goolisfan@gmail.com>
2017-10-25 17:22:52 +0800

    feat: 使用自定义的server服务，webpack作用中间件服务

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,13 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   devtool: 'inline-source-map',
   devServer: {
-       contentBase: './dist'
-   },
+    contentBase: './dist'
+  },
   module: {
     rules: [
       {
