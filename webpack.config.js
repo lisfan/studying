ac3edbc129ed2cf514035c8c04ad73c3293b3ce4
lisfan <goolisfan@gmail.com>
2017-11-17 17:18:11 +0800

    fix: 暂时提交

--- webpack.config.js
+++ webpack.config.js
@@ -5,63 +5,66 @@ const ManifestPlugin = require('webpack-manifest-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const webpack = require('webpack')
-console.log('NODE_ENV: ', process.env.NODE_ENV) // 'local'
-console.log('Production: ', process.env.production) // true
-console.log('xxxx: ', process.env.xxxx) // true
 
-module.exports = {
-  entry: {
-    index: './src/index.js',
-    vender: ['lodash'],
-  },
-  output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist'),
-    chunkFilename: '[name].bundle.js',
-    library: 'webpackNumbers',
-    libraryTarget: 'umd',
-    publicPath: '/studying/dist'
-  },
-  // externals: ['lodash', 'jquery'],
-  // devtool: 'inline-source-map',
-  // devServer: {
-  //   contentBase: './dist',
-  //   // hot: true
-  // },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.png?$/,
-        use: {
-          loader: 'file-loader',
+module.exports = function (env) {
+  console.log('NODE_ENV: ', env.NODE_ENV) // 'local'
+  console.log('Production: ', env.production) // true
+  console.log('xxxx: ', env.xxxx) // true
+
+  return {
+    entry: {
+      index: './src/index.js',
+      vender: ['lodash'],
+    },
+    output: {
+      filename: '[name].[hash].js',
+      path: path.resolve(__dirname, 'dist'),
+      chunkFilename: '[name].bundle.js',
+      library: 'webpackNumbers',
+      libraryTarget: 'umd',
+      publicPath: '/studying/dist'
+    },
+    // externals: ['lodash', 'jquery'],
+    // devtool: 'inline-source-map',
+    // devServer: {
+    //   contentBase: './dist',
+    //   // hot: true
+    // },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.png?$/,
+          use: {
+            loader: 'file-loader',
+          },
         },
-      },
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            happyPackMode: true,
-            transpileOnly: true
-          }
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              happyPackMode: true,
+              transpileOnly: true
+            }
+          },
+          exclude: /node_modules/,
         },
-        exclude: /node_modules/,
-      },
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Code Splitting'
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vender",
-    })
-    // new ManifestPlugin(),
-    // new webpack.HotModuleReplacementPlugin(),
-    // new UglifyJSPlugin(),
-  ],
-};
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        title: 'Code Splitting'
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "vender",
+      })
+      // new ManifestPlugin(),
+      // new webpack.HotModuleReplacementPlugin(),
+      // new UglifyJSPlugin(),
+    ],
+  }
+}
